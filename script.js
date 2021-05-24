553963f91c58c785643914d218f197b3e3506fe8
Alireza Pirmozhdehi <zootel.land@gmail.comi>
2021-05-24 13:07:52 +0430

    Project started

---
+++ script.js
@@ -0,0 +1,66 @@
+const foods = [
+  {
+    id: 0,
+    name: "همبرگر مخصوص",
+    price: 10000,
+    src: "./img/hamburger.png",
+  },
+  {
+    id: 1,
+    name: "همبرگر معمولی",
+    price: 8000,
+    src: "./img/hamburger.png",
+  },
+  {
+    id: 2,
+    name: "همبرگر مخصوص با قارچ و پنیر",
+    price: 20000,
+    src: "./img/hamburger.png",
+  },
+  {
+    id: 3,
+    name: "همبرگر معمولی با قارچ و پنیر",
+    price: 10000,
+    src: "./img/hamburger.png",
+  },
+  {
+    id: 4,
+    name: "سیب زمینی سرخ کرده",
+    price: 10000,
+    src: "./img/french_fries.png",
+  },
+  {
+    id: 5,
+    name: "سیب زمینی سرخ کرده ویژه",
+    price: 25000,
+    src: "./img/french_fries.png",
+  },
+  {
+    id: 6,
+    name: "نوشابه",
+    price: 5000,
+    src: "./img/soda.png",
+  },
+  {
+    id: 7,
+    name: "نوشابه رژیمی",
+    price: 6000,
+    src: "./img/soda.png",
+  },
+  {
+    id: 8,
+    name: "سالاد سزار",
+    price: 25000,
+    src: "./img/ceasar.png",
+  },
+  {
+    id: 9,
+    name: "سالاد فصل",
+    price: 8000,
+    src: "./img/salad.png",
+  },
+];
+
+const renderFoods = (foods) => {
+    foods
+};
